672f85377f712c73501dab8f0b1ef7548f1e8b1d
Machamasi <sundar.machamasi@capgemini.com>
2024-08-22 14:46:39 +0900

    Admin - Create & Save Product

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,7 +1,17 @@
+import Product from "../models/product.js";
 
-
+// Create new Product => /api/v1/products
 export const getProducts = async ( req, res ) => {
     res.status(200).json({
         message: "All Products",
+    });
+};
+
+// Create new Product => /api/v1/admin/products
+export const newProduct = async ( req, res ) => {
+    const product = await Product.create(req.body);
+
+    res.status(200).json({
+        product,
     });
 };
