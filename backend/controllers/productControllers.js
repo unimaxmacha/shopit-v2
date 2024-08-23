ed7f83fbc58c8e34131cf814ec657b1738bf8fdd
Machamasi <sundar.machamasi@capgemini.com>
2024-08-23 10:12:52 +0900

    Filter by Keyword

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,13 +1,19 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import Product from "../models/product.js";
+import APIFilters from "../utils/apiFilters.js";
 import ErrorHandler from "../utils/errorHandler.js";
 
 // Create new Product => /api/v1/products
 export const getProducts = catchAsyncErrors(async ( req, res ) => {
 
-    const products = await Product.find();
+    const apiFilters = new APIFilters(Product, req.query).search();
+
+    let products = await apiFilters.query;
+    let filteredProductsCount = products.length;
+
     res.status(200).json({
-       products,
+        filteredProductsCount,
+        products,
     });
 });
 
