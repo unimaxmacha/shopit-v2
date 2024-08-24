25ad744406f10bf890119677d65110479d00a080
Machamasi <sundar.machamasi@capgemini.com>
2024-08-24 09:32:03 +0900

    Generate web Token with issue

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
     {
@@ -41,5 +42,12 @@ userSchema.pre("save", async function (next) {
 
     this.password = await bcrypt.hash(this.password, 10);
 });
+
+// Return JWT TOken
+userSchema.methods.getJwtToken = function() {
+    return jwt.sign({ id: this._id}, procress.env.JWT_SECRET, {
+        expiresIn : process.env.JWT_EXPIRES_TIME,
+    });
+};
 
 export default mongoose.model("User", userSchema);
