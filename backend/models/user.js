5f05f40fce99e94fecd31255ac77b6dcc72239c7
Machamasi <sundar.machamasi@capgemini.com>
2024-08-26 13:17:13 +0900

    Generate forget password token

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema = new mongoose.Schema(
     {
@@ -53,6 +54,24 @@ userSchema.methods.getJwtToken = function() {
 // Compare user password
 userSchema.methods.comparePassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Generate password reset token
+userSchema.methods.getResetPasswordToken = function () {
+
+    // Generate token
+    const resetToken = crypto.randomBytes(20).toString("hex");
+
+    // Hash and set to resetPasswordToken field
+    this.resetPasswordToken = crypto
+        .createHash("sha256")
+        .update(resetToken)
+        .digest("hex");
+
+    // Set token expire time
+    this.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
+
+    return resetToken;
 };
 
 export default mongoose.model("User", userSchema);
