d518e38e04b53cd3bcf52b5f7dfb2d8797efb0d7
Machamasi <sundar.machamasi@capgemini.com>
2024-08-26 12:14:13 +0900

    Authorize User Role and Permission

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -17,3 +17,20 @@ export const isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
     
     next();
 });
+
+// Authorize user roles
+export const authorizeRoles = (...roles) => {
+    return (req, res, next ) => {
+        if (!roles.includes(req.user.role)) {
+            return next(
+                new ErrorHandler(
+                    `Role (${req.user.role}) is not allowed to access this resources`,
+                    403,
+                ),
+            );
+        };
+
+        next();
+
+    };
+};
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,12 +1,22 @@
 import express from "express";
-import { deleteProduct, getProductDetails, getProducts, newProduct, updateProductDetails } from "../controllers/productControllers.js";
-import { isAuthenticatedUser } from "../middlewares/auth.js";
+import { 
+    deleteProduct, 
+    getProductDetails, 
+    getProducts, 
+    newProduct, 
+    updateProductDetails 
+} from "../controllers/productControllers.js";
+import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
 const router = express.Router();
 
-router.route("/products").get(isAuthenticatedUser, getProducts);
+router
+    .route("/products")
+    .get(isAuthenticatedUser, authorizeRoles("admin"), getProducts);
 router.route("/admin/products").post(newProduct);
+
 router.route("/products/:id").get(getProductDetails);
-router.route("/products/:id").put(updateProductDetails);
-router.route("/products/:id").delete(deleteProduct);
+
+router.route("/admin/products/:id").put(updateProductDetails);
+router.route("/admin/products/:id").delete(deleteProduct);
 
 export default router;
