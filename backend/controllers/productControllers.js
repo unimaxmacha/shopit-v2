b5dc1ecedf88da9b9e9e712ca0ed7e01b2cbb9cf
Machamasi <sundar.machamasi@capgemini.com>
2024-08-27 23:09:55 +0900

    52. Add New/Update Review

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -77,3 +77,46 @@ export const deleteProduct = catchAsyncErrors(async ( req, res ) => {
         message: "Product deleted.",
     });
 });
+
+// Create/Update product review => /api/v1/reviews
+export const createProductReview = catchAsyncErrors(async ( req, res ) => {
+
+    const { rating, comment, productId } = req.body;
+
+    const review = {
+        user: req?.user?._id,
+        rating: Number(rating),
+        comment,
+    };
+
+    const product = await Product.findById(productId);
+
+    if(!product) {
+        return next (new ErrorHandler("Product not found.", 404));
+    };
+
+    const isReviewed = product?.reviews?.find(
+        (r) => r.user.toString() ===req?.user?._id.toString()
+    );
+
+    if(isReviewed) {
+        product.reviews.forEach((review) => {
+            if (review?.user?.toString() === req?.user?._id.toString()) {
+                review.comment = comment;
+                review.rating = rating;
+            }
+        });
+    } else {
+        product.reviews.push(review)
+        product.numOfReviews = product.reviews.length;
+    }
+
+    product.ratings = product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+    product.reviews.length;
+
+    await product.save({ validateBeforeSave: false });
+
+    res.status(200).json({
+        success: true,
+    });
+});
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { 
+    createProductReview,
     deleteProduct, 
     getProductDetails, 
     getProducts, 
@@ -23,5 +24,7 @@ router
 router
     .route("/admin/products/:id")
     .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+
+router.route("/reviews").put(isAuthenticatedUser, createProductReview);
 
 export default router;
