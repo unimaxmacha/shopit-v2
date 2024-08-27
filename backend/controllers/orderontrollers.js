fb0160791539ce72fb11e63a81346cfaf5d24a0a
Machamasi <sundar.machamasi@capgemini.com>
2024-08-27 14:30:03 +0900

    49. Get Single Order & Logged in User Orders

--- backend/controllers/orderontrollers.js
+++ backend/controllers/orderontrollers.js
@@ -31,3 +31,28 @@ export const newOrder = catchAsyncErrors(async (req, res, next) => {
         order,
     });
 });
+
+// Get current user's orders=> /api/v1/me/orders
+export const myOrders = catchAsyncErrors(async (req, res, next) => {
+    const orders = await Order.find({ user: req.user._id });
+
+    res.status(200).json({
+        orders,
+    });
+});
+
+// Get order details=> /api/v1/orders/:id
+export const getOrderDetails = catchAsyncErrors(async (req, res, next) => {
+    const order = await Order.findById(req.params.id).populate(
+        "user", 
+        "name email",
+    );
+
+    if(!order) {
+        return next (new ErrorHandler ("Order not found with this ID.", 404));
+    }
+
+    res.status(200).json({
+        order,
+    });
+});
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,9 +1,15 @@
 import express from "express";
 const router = express.Router();
 
-import { newOrder } from "../controllers/orderontrollers.js";
+import { 
+    getOrderDetails, 
+    myOrders, 
+    newOrder,
+} from "../controllers/orderontrollers.js";
 import { isAuthenticatedUser } from "../middlewares/auth.js";
 
-router.route("/orders/new").post(isAuthenticatedUser, newOrder)
+router.route("/orders/new").post(isAuthenticatedUser, newOrder);
+router.route("/orders/:id").get(isAuthenticatedUser, getOrderDetails);
+router.route("/me/orders").get(isAuthenticatedUser, myOrders);
 
 export default router;
