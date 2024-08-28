6ecceb2f488b687e71e4743ef34df78064653cd6
Sundar Machamasi <unimaxmacha@gmail.com>
2024-08-28 22:31:34 +0900

    66. Adding React Hot Toast

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,17 @@ import "./App.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Home from "./components/Home";
+
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
-import Home from "./components/Home";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Toaster position="top-center" />
         <Header />
         <div className="container">
           <Routes>
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MetaData from "./layout/MetaData";
 import { useGetProductsQuery } from "../redux/api/productsApi";
 import ProductItem from "./product/ProductItem";
+import Loader from "./layout/Loader";
+import toast from "react-hot-toast";
 
 const Home = () => {
 
-    const { data, isLoading } = useGetProductsQuery();
+    const { data, isLoading, error, isError } = useGetProductsQuery();
+
+    useEffect(() => {
+        if(isError) {
+            toast.error(error?.data?.message);
+        }
+    }, [isError]);
     
-    console.log(data, isLoading);
+    if(isLoading) return <Loader />
     
     return (
         <>
