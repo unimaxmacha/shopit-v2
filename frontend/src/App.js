a34cd83e4848b12fe13acfe172126f91178de721
CG-Sundar <sundar.machamasi@capgemini.com>
2024-09-03 10:08:21 +0900

    74. Register User

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import Header from "./components/layout/Header";
 import { Toaster } from "react-hot-toast";
 import ProductDetails from "./components/product/ProductDetails";
 import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/product/:id" element={<ProductDetails />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </div>
         <Footer />
--- frontend/src/components/auth/Register.jsx
+++ frontend/src/components/auth/Register.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { useLoginMutation } from "../../redux/api/authApi";
+import { useRegisterMutation } from "../../redux/api/authApi";
 import toast from "react-hot-toast";
 
-const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+const Register = () => {
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
 
-    const[login, { isLoading, error, data }] = useLoginMutation();
+    const { name, email, password } = user;
+
+    const [register, { isLoading, error, data }]= useRegisterMutation();
 
     useEffect(() => {
         if (error) {
@@ -17,19 +22,40 @@ const Login = () => {
     const submitHandler = (e) => {
         e.preventDefault();
 
-        const loginData = {
+        const signUpData = {
+            name,
             email,
             password,
         };
 
-        login(loginData);
+        register(signUpData);
+    };
+
+    const onChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value });
     };
 
     return (
         <div className="row wrapper">
             <div className="col-10 col-lg-5">
-                <form className="shadow rounded bg-body" onSubmit={submitHandler} >
-                    <h2 className="mb-4">Login</h2>
+                <form
+                    className="shadow rounded bg-body"
+                    onSubmit={submitHandler}
+                >
+                    <h2 className="mb-4">Register</h2>
+
+                    <div className="mb-3">
+                        <label htmlFor="name_field" className="form-label">Name</label>
+                        <input
+                            type="text"
+                            id="name_field"
+                            className="form-control"
+                            name="name"
+                            value={name}
+                            onChange={onChange}
+                        />
+                    </div>
+
                     <div className="mb-3">
                         <label htmlFor="email_field" className="form-label">Email</label>
                         <input
@@ -38,7 +64,7 @@ const Login = () => {
                             className="form-control"
                             name="email"
                             value={email}
-                            onChange={(e) => setEmail(e.target.value)}
+                            onChange={onChange}
                         />
                     </div>
 
@@ -50,28 +76,22 @@ const Login = () => {
                             className="form-control"
                             name="password"
                             value={password}
-                            onChange={(e) => setPassword(e.target.value)}
+                            onChange={onChange}
                         />
                     </div>
 
-                    <a href="/password/forgot" className="float-end mb-4">Forgot Password?</a>
-
                     <button 
-                        id="login_button" 
+                        id="register_button" 
                         type="submit" 
-                        className="btn w-100 py-2" 
+                        className="btn w-100 py-2"
                         disabled={isLoading}
                     >
-                        {isLoading ? "Authenticating..." : "LOGIN"}
+                        {isLoading ? "Creating..." : "REGISTER"}
                     </button>
-
-                    <div className="my-3">
-                        <a href="/register" className="float-end">New User?</a>
-                    </div>
                 </form>
             </div>
         </div>
     );
 };
 
-export default Login;
+export default Register;
