1d0b82e816fff7eb51fd78c65306b3102afaef5f
CG-Sundar <sundar.machamasi@capgemini.com>
2024-09-05 16:16:55 +0900

    83. Fix Image Size Bug

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -5,7 +5,7 @@ import ErrorHandler from "../utils/errorHandler.js";
 import sendToken from "../utils/sendToken.js";
 import sendEmail from "../utils/sendEmail.js";
 import crypto from "crypto";
-import { upload_file } from "../utils/cloudinary.js";
+import { delete_file, upload_file } from "../utils/cloudinary.js";
 
 // Register User => /api/v1/register
 export const registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -60,6 +60,11 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
 // Upload user avatar => /api/v1/me/upload_avatar
 export const uploadAvatar = catchAsyncErrors(async (req, res, next) => {
     const avatarResponse = await upload_file(req.body.avatar, "shopIT/avatars");
+
+    // Remove previous avatar
+    if (req?.user?.avatar?.url) {
+        await delete_file(req?.user?.avatar?.public_id);
+    }
 
     const user = await User.findByIdAndUpdate(req?.user?._id, {
         avatar: avatarResponse,
