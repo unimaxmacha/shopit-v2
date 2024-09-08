1d57b294704331e99783d2d3cb559759008c393a
CG-Sundar <sundar.machamasi@capgemini.com>
2024-09-08 15:21:37 +0900

    95. Checkout Steps Component

--- frontend/src/components/cart/Shipping.jsx
+++ frontend/src/components/cart/Shipping.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { saveShippingInfo } from "../../redux/features/cartSlice";
 import { useNavigate } from "react-router-dom";
 import MetaData from "../layout/MetaData";
+import CheckoutSteps from "./CheckoutSteps";
 
 const Shipping = () => {
     const countriesList = Object.values(countries);
@@ -39,6 +40,7 @@ const Shipping = () => {
     return (
         <>
             <MetaData title={"Shipping Info"} />
+            <CheckoutSteps shipping />
             <div className="row wrapper mb-5">
                 <div className="col-10 col-lg-5">
                     <form className="shadow rounded bg-body" onSubmit={submitHandler} >
