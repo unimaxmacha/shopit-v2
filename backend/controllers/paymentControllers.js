f727c4872dd00b5b23b20f02c21e7af9d02ddc1d
Sundar Machamasi <unimaxmacha@gmail.com>
2024-09-09 15:30:18 +0900

    101. Place on Successful Payment

--- backend/controllers/paymentControllers.js
+++ backend/controllers/paymentControllers.js
@@ -53,3 +53,67 @@ export const stripeCheckoutSession = catchAsyncErrors(async (
         });
     }
 );
+
+const getOrderItems = async (line_items) => {
+    return new Promise((resolve, reject) => {
+        let cartItems = [];
+
+        line_items?.data?.forEach(async (item) => {
+            const product = await stripe.products.retrive(item.price.product);
+            const productId = product.metadata.productId;
+
+            console.log("===================================");
+            console.log("My Console item =>", item);
+            console.log("===================================");
+            console.log("My Console product =>", product);
+
+            cartItems.push({
+                product: productId,
+                name: product.name,
+                price: item.price.unit_amount_decimal / 100,
+                quantity: item.quantity,
+                image: product.images[0],
+            });
+
+            if (cartItems.length === line_item?.data?.length) {
+                resolve(cartItems);
+            }
+        });
+    });
+};
+
+// Create new order after payment => /api/v1/payment/webhook
+export const stripeWebhook = catchAsyncErrors(async (
+    req, res, next) => {
+        try {
+            const signature = req.headers["stripe-signature"];
+
+            const event = stripe.webhooks.constructEvent(
+                req.rawBody, 
+                signature, 
+                process.env.STRIPE_WEBHOOK_SECRET
+            );
+
+            if (event.type === "checkout.session.completed") {
+                const session = event.data.object;
+
+                const line_items = await stripe.checkout.sessions.listLineItems (
+                    session.id
+                );
+
+                const orderItems = await getOrderItems(line_items);
+
+                console.log("===================================");
+                console.log("My Console =>", orderItems);
+                console.log("===================================");
+
+                res.status(200).json({ success: true});
+            }
+        } catch (error) {
+            console.log("===================================");
+            console.log("My Console =>", error);
+            console.log("===================================");
+        }
+    
+    }
+);
