3861997d4a073535ae58af97795c3584e4583d9f
CG-Sundar <sundar.machamasi@capgemini.com>
2024-09-13 00:20:27 +0900

    107. Display all Reviews

--- frontend/src/components/product/ProductDetails.jsx
+++ frontend/src/components/product/ProductDetails.jsx
@@ -8,6 +8,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setCartItem } from "../../redux/features/cartSlice";
 import MetaData from "../layout/MetaData";
 import NewReviews from "../reviews/NewReview";
+import ListReviews from "../reviews/ListReviews";
 
 const ProductDetails = () => {
     const params = useParams();
@@ -175,6 +176,9 @@ const ProductDetails = () => {
                     )}
                 </div>
             </div>
+            { product?.reviews?.length > 0 && (
+                <ListReviews reviews={product?.reviews} />
+            )}
         </>
     );
 };
--- frontend/src/redux/api/productsApi.js
+++ frontend/src/redux/api/productsApi.js
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const productApi = createApi({
     reducerPath: "productApi",
     baseQuery: fetchBaseQuery({ baseUrl: "/api/v1" }),
+    tagTypes: ["Product"],
     endpoints: (builder) => ({
         getProducts: builder.query({
             query: (params) => ({
@@ -19,6 +20,7 @@ export const productApi = createApi({
         }),
         getProductDetails: builder.query({
             query: (id) => `/products/${id}`,
+            providesTags: ["Product"],
         }),
         submitReview: builder.mutation({
             query(body) {
@@ -28,6 +30,7 @@ export const productApi = createApi({
                     body,
                 };
             },
+            invalidatesTags: ["Product"],
         }),
     }),
 });
