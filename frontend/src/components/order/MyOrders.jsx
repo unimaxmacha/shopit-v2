c06874a42dec921d14aeebbbf6e05e222939b406
CG-Sundar <sundar.machamasi@capgemini.com>
2024-09-12 14:35:59 +0900

    104. Clear Cart after Order

--- frontend/src/components/order/MyOrders.jsx
+++ frontend/src/components/order/MyOrders.jsx
@@ -3,18 +3,30 @@ import { useMyOrdersQuery } from "../../redux/api/orderApi";
 import Loader from "../layout/Loader";
 import toast from "react-hot-toast";
 import { MDBDataTable } from "mdbreact";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import MetaData from "../layout/MetaData";
+import { useDispatch } from "react-redux";
+import { clearCart } from "../../redux/features/cartSlice";
 
 const MyOrder = () => {
-
     const { data, isLoading, error } = useMyOrdersQuery();
+
+    const [searchParams] = useSearchParams();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const orderSuccess = searchParams.get("order_success");
 
     useEffect(() => {
         if (error) {
             toast.error(error?.data?.message);
         }
-    }, [error]);
+
+        if(orderSuccess) {
+            dispatch(clearCart());
+            navigate("/me/orders");
+        }
+    }, [error, orderSuccess]);
 
     const setOrders = () => {
         const orders = {
--- frontend/src/redux/features/cartSlice.js
+++ frontend/src/redux/features/cartSlice.js
@@ -40,6 +40,11 @@ export const cartSlice = createSlice({
             localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
         },
 
+        clearCart: (state, action) => {
+            localStorage.removeItem("cartItems");
+            state.cartItems = [];
+        },
+
         saveShippingInfo: (state, action) => {
             state.shippingInfo = action.payload
 
@@ -50,4 +55,4 @@ export const cartSlice = createSlice({
 
 export default cartSlice.reducer;
 
-export const { setCartItem, removeCartItem, saveShippingInfo } = cartSlice.actions;
+export const { setCartItem, removeCartItem, clearCart, saveShippingInfo } = cartSlice.actions;
